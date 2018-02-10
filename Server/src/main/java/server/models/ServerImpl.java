ad369f9fbfd3e5245869e355a671fff5e87f9c24
Hasnaa Mohammed <Hasnaa Mohammed@DESKTOP-27E46FO>
2018-02-10 12:55:19 +0200

    DB connection single tone class implementation for sign up implementation for log in default User Constructor

--- Server/src/main/java/server/models/ServerImpl.java
+++ Server/src/main/java/server/models/ServerImpl.java
@@ -66,7 +66,7 @@ public class ServerImpl implements ServerInter {
 
     @Override
     public void sendMessage(ClientInter sender, ClientInter receiver) throws RemoteException {
-            receiver.recieveMessage(sender.getUser());
+           receiver.recieveMessage(sender.getUser());
     }
 
     @Override
