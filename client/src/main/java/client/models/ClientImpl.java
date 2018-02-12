44112a0064016695e28b17da88afe6ed1495164d
AhmED Nagib <ahmed.nagib93@gmail.com>
2018-02-12 11:12:49 +0200

    make clint interface implemenation remote

--- client/src/main/java/client/models/ClientImpl.java
+++ client/src/main/java/client/models/ClientImpl.java
@@ -8,53 +8,55 @@ package client.models;
 import iti.chat.common.ClientInter;
 import iti.chat.common.Message;
 import iti.chat.common.User;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  *
  * @author Hasnaa Mohammed
  */
-public class ClientImpl implements ClientInter{
+public class ClientImpl extends UnicastRemoteObject implements ClientInter {
+
     User user;
     Message msg;
-    
+
+    public ClientImpl() throws RemoteException {
+        this.user = null;
+    }
+
+    public ClientImpl(User user) throws RemoteException {
+        this.user = user;
+    }
+
     @Override
-    public void recieveMessage(User sender) {
+    public void recieveMessage(User sender) throws RemoteException {
         System.out.println(msg.getContent());
     }
-    
-    public void setUser(User usr){
-        user=usr;
+
+    public void setUser(User usr) {
+        user = usr;
     }
+
     @Override
-    public User getUser() {
-       return user;
+    public User getUser() throws RemoteException {
+        return user;
     }
-    
-     public void setMessage(Message message){
-        msg=message;
+
+    public void setMessage(Message message) {
+        msg = message;
     }
 
     public Message getMessage() {
-       return msg;
+        return msg;
     }
-    
-    /*Methods added by Nagib  */
-    
-    
- /*Methods added by Dina  */
 
+    /*Methods added by Nagib  */
+ /*Methods added by Dina  */
 //    
-    
- /*Methods added by Hassna  */
-    
+    /*Methods added by Hassna  */
     //
-    
- /*Methods added by Hesham  */
-    
+    /*Methods added by Hesham  */
     //
-    
- /*Methods added by Fatma  */
-    
+    /*Methods added by Fatma  */
     //
-    
 }
