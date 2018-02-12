b39c2b39c5d31ac4dd2dc1a401a43ccb2057871b
AhmED Nagib <ahmed.nagib93@gmail.com>
2018-02-12 11:49:07 +0200

    solve conflict

--- client/src/main/java/client/models/ClientImpl.java
+++ client/src/main/java/client/models/ClientImpl.java
@@ -39,7 +39,7 @@ public class ClientImpl extends UnicastRemoteObject implements ClientInter {
 
     @Override
     public User getUser() throws RemoteException {
-        return user;
+        return this.user;
     }
 
     public void setMessage(Message message) {
@@ -53,30 +53,26 @@ public class ClientImpl extends UnicastRemoteObject implements ClientInter {
     /*Methods added by Nagib  */
     //
     //
-    
- /*Methods added by Dina  */
- //
- //
- //    
-    
- /*Methods added by Hassna  */
-    
-    public boolean signOut(){
+    /*Methods added by Dina  */
+    //
+    //
+    //    
+    /*Methods added by Hassna  */
+    public boolean signOut() {
         boolean done = false;
         //serverObject.signOurServer(this.user.email);
         return done;
     }
-        @Override
-    public void getNotification(String content) {
-        //serverObject.displayNotification(content);
+
+    @Override
+    public void getNotification(String content) throws RemoteException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
- /*Methods added by Hesham  */
+
+    /*Methods added by Hesham  */
     //
     //
-    
- /*Methods added by Fatma  */
+    /*Methods added by Fatma  */
     //
     //
     //
